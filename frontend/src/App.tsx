e86f7358b09d6bca39a9a2fdd9757fa50f0c9d4b
Sushanth Krishnaswamy <sushanth.k4@gmail.com>
2026-02-24 10:07:57 -0500

    initial version of the monorepo with backend dockerized fastapi app and frontend non-dockerized react app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,15 @@ import { Layout } from "./components/layout/Layout"
 import { Tasks } from "./pages/Tasks"
 import { Notes } from "./pages/Notes"
 import { ThemeProvider } from "./components/theme-provider"
+import { useEffect } from "react"
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:8000/api/health')
+      .then(res => res.json())
+      .then(data => console.log('Backend health:', data))
+      .catch(err => console.error('Backend connection error:', err));
+  }, []);
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <BrowserRouter>
